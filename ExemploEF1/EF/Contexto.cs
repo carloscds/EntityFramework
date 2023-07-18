067a2d7125777cc39afcdec13d60cf539786d947
Carlos dos Santos <cds@cds-software.com.br>
2023-07-17 21:28:10 -0300

    Inicio EF

---
+++ ExemploEF1/EF/Contexto.cs
@@ -0,0 +1,29 @@
+﻿using ExemploEF1.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploEF1.EF
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options): base(options) {}
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>()
+                .HasKey(p => p.ID);
+            modelBuilder.Entity<Cliente>()
+                .Property(p => p.ID)
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ExemploEF1/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExemploEF1.Domain;
+using ExemploEF1.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExemploEF1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var opt = new DbContextOptionsBuilder<DbContext>()
+                .UseSqlServer("data source=(local); initial catalog=ExemploEF;integrated security=true;trusted_connection=true; encrypt=false;")
+                .LogTo(l => Console.WriteLine(l));
+            var db = new Contexto(opt.Options);
+
+            var cli = new Cliente { Nome = "Carlos" };
+            db.Cliente.Add(cli);
+            db.SaveChanges();
+
+            var dados = db.Cliente.Where(w => w.ID > 0);
+            foreach(var c in dados)
+            {
+                Console.WriteLine(c.Nome);
+            }
+        }
+    }
+}
