98ed818e289eb256fa13553c7fdc6fb00b966ab5
Carlos dos Santos <cds@cds-software.com.br>
2023-07-25 23:08:25 -0300

    EF Core PowerTools

---
+++ EngenhariaReversa/Program.cs
@@ -0,0 +1,20 @@
+﻿using EngenhariaReversa.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EngenhariaReversa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var opt = new DbContextOptionsBuilder<EF1Context>()
+                .UseSqlServer("data source=(local); initial catalog=EF1;integrated security=true;trusted_connection=true; encrypt=false;");
+            var db = new EF1Context(opt.Options);
+
+            foreach(var c in db.Cliente) 
+            {
+                Console.WriteLine($"{c.Nome}");
+            }
+        }
+    }
+}
